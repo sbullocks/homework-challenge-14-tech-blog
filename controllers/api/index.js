ad144c4e0992159996ab2b3d1669bb97a21bd156
sbullocks <sbullocks@gmail.com>
2022-11-10 16:21:54 -0500

    fixed duplicated post route and changed 1 to comment route.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
-const postRoutes = require('./comment-routes');
+const commentRoutes = require('./comment-routes');
 const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
