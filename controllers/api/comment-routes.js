964ceaac9b82f16f4bdf1f966fd2a16ee4d18e7d
sbullocks <sbullocks@gmail.com>
2022-11-09 23:41:05 -0500

    updated comment routes in api controller.

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,1 +1,29 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
+// CREATE new user
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const latestComment = await Comment.create({
+    //   username: req.body.username,
+    //   email: req.body.email,
+    //   password: req.body.password,
+    ...req.body,
+    user_id: req.session.user_id,
+    });
+    res.json(latestComment);
+
+    // Set up sessions with a 'loggedIn' variable set to `true`
+    // req.session.save(() => {
+    //   req.session.loggedIn = true;
+
+    //   res.status(200).json(latestUser);
+    // });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
