e8a8cc25adfd27ed4fc3e15087d7e46be971eeff
sbullocks <sbullocks@gmail.com>
2022-11-09 23:40:19 -0500

    updated user file in seeds.

---
+++ seeds/userSeed.js
@@ -0,0 +1,13 @@
+const { User } = require('../models');
+
+const userSeed = [
+  {
+    username: 'testUsername',
+    email: 'testemail@gmail.com',
+    password: 'testpassword',
+  },
+];
+
+const seedUsers = () => User.bulkCreate(userSeed);
+
+module.exports = seedPost;
