2de9c89ce1d650d75c1113ce6e4bd49728ef12a0
sbullocks <sbullocks@gmail.com>
2022-11-09 23:42:49 -0500

    updated login file for public foler.

---
+++ public/assets/js/login.js
@@ -0,0 +1,30 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (name && email && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+  
+  
