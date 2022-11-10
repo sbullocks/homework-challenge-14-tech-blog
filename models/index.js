f14e9acaef4f0edcd773cec9896067ee0c60ed4e
sbullocks <sbullocks@gmail.com>
2022-11-09 23:37:36 -0500

    updated associations on index.js in models.

---
+++ models/index.js
@@ -0,0 +1,40 @@
+const User = require('./User');
+const Comment = require('./Comment');
+const Post = require('./Post');
+
+// Associations
+
+// User has many comment
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+// User has many Post
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+// Comment belongs to User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Comment belongs to Post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+// Post belongs to User
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Post has many Comment
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Project };
